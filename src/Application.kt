8e2c10af6bbd5e132644d39c0c221841f92fa5af
ken <dcfn4@163.com>
2020-03-02 00:25:46 +0800

    ktor init

---
+++ src/Application.kt
@@ -0,0 +1,112 @@
+package com.phoenix
+
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import com.fasterxml.jackson.databind.SerializationFeature
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.auth.Authentication
+import io.ktor.auth.UserIdPrincipal
+import io.ktor.auth.authenticate
+import io.ktor.auth.jwt.jwt
+import io.ktor.auth.principal
+import io.ktor.features.CORS
+import io.ktor.features.ContentNegotiation
+import io.ktor.features.StatusPages
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpMethod
+import io.ktor.http.HttpStatusCode
+import io.ktor.jackson.jackson
+import io.ktor.request.receive
+import io.ktor.response.respond
+import io.ktor.routing.get
+import io.ktor.routing.post
+import io.ktor.routing.route
+import io.ktor.routing.routing
+import java.util.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+fun Application.module() {
+    val simpleJwt = SimpleJWT("my-super-secret-for-jwt")
+    install(CORS) {
+        method(HttpMethod.Options)
+        method(HttpMethod.Get)
+        method(HttpMethod.Post)
+        method(HttpMethod.Put)
+        method(HttpMethod.Delete)
+        method(HttpMethod.Patch)
+        header(HttpHeaders.Authorization)
+        allowCredentials = true
+        anyHost()
+    }
+    install(StatusPages) {
+        exception<InvalidCredentialsException> { exception ->
+            call.respond(HttpStatusCode.Unauthorized, mapOf("OK" to false, "error" to (exception.message ?: "")))
+        }
+    }
+    install(Authentication) {
+        jwt {
+            verifier(simpleJwt.verifier)
+            validate {
+                UserIdPrincipal(it.payload.getClaim("name").asString())
+            }
+        }
+    }
+    install(ContentNegotiation) {
+        jackson {
+            enable(SerializationFeature.INDENT_OUTPUT) // Pretty Prints the JSON
+        }
+    }
+    routing {
+        post("/login-register") {
+            val post = call.receive<LoginRegister>()
+            val user = users.getOrPut(post.user) { User(post.user, post.password) }
+            if (user.password != post.password) throw InvalidCredentialsException("Invalid credentials")
+            call.respond(mapOf("token" to simpleJwt.sign(user.name)))
+        }
+        route("/snippets") {
+            get {
+                call.respond(mapOf("snippets" to synchronized(snippets) { snippets.toList() }))
+            }
+            authenticate {
+                post {
+                    val post = call.receive<PostSnippet>()
+                    val principal = call.principal<UserIdPrincipal>() ?: error("No principal")
+                    snippets += Snippet(principal.name, post.snippet.text)
+                    call.respond(mapOf("OK" to true))
+                }
+            }
+        }
+    }
+}
+
+data class PostSnippet(val snippet: PostSnippet.Text) {
+    data class Text(val text: String)
+}
+
+data class Snippet(val user: String, val text: String)
+
+val snippets = Collections.synchronizedList(mutableListOf(
+    Snippet(user = "test", text = "hello"),
+    Snippet(user = "test", text = "world")
+))
+
+open class SimpleJWT(val secret: String) {
+    private val algorithm = Algorithm.HMAC256(secret)
+    val verifier = JWT.require(algorithm).build()
+    fun sign(name: String): String = JWT.create().withClaim("name", name).sign(algorithm)
+}
+
+class User(val name: String, val password: String)
+
+val users = Collections.synchronizedMap(
+    listOf(User("test", "test"))
+        .associateBy { it.name }
+        .toMutableMap()
+)
+
+class InvalidCredentialsException(message: String) : RuntimeException(message)
+
+class LoginRegister(val user: String, val password: String)
